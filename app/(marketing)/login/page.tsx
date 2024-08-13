3d66496086cd640dd3611e2319f184a9682aa8f4
flcampanelli <flcampanelli@gmail.com>
2024-08-12 21:30:43 -0300

    feat(auth): add auth provider and auth button components, integrate with layout and pages

--- app/(marketing)/login/page.tsx
+++ app/(marketing)/login/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Command } from "lucide-react";
 
 import { UserLoginForm } from "@/components/user-login-form";
+import AuthButton from "@/components/auth-button";
 
 export const metadata: Metadata = {
   title: "Autenticação",
@@ -14,10 +15,11 @@ export const metadata: Metadata = {
 export default async function Login() {
   return (
     <div className="container relative py-48 md:py-0  h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <AuthButton page="login" />
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
-          <Command className="mr-2 h-6 w-6" /> Authentication
+          <Command className="mr-2 h-6 w-6" /> Login
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
@@ -35,7 +37,7 @@ export default async function Login() {
           </div>
           <UserLoginForm />
           <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{" "}
+            Ao clicar em continuar, você concorda com nossos{" "}
             <Link
               href="/terms"
               className="underline underline-offset-4 hover:text-primary"
--- app/(marketing)/register/page.tsx
+++ app/(marketing)/register/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Command } from "lucide-react";
 
 import { UserRegisterForm } from "@/components/user-register-form";
+import AuthButton from "@/components/auth-button";
 
 export const metadata: Metadata = {
   title: "Cadastro",
@@ -14,6 +15,7 @@ export const metadata: Metadata = {
 export default async function Register() {
   return (
     <div className="container relative py-48 md:py-0  h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <AuthButton page="register" />
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
@@ -37,7 +39,7 @@ export default async function Register() {
           </div>
           <UserRegisterForm />
           <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{" "}
+            Ao clicar em continuar, você concorda com nossos{" "}
             <Link
               href="/terms"
               className="underline underline-offset-4 hover:text-primary"

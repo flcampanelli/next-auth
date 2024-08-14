33ede3146fc0a40fe92dcf8d1ca8424c46f366c0
flcampanelli <flcampanelli@gmail.com>
2024-08-13 21:31:37 -0300

    feat(auth): add server component auth to display user data in server-side component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import ClientComponentAuth from "@/components/client-component-auth";
+import ServerComponentAuth from "@/components/server-component-auth";
 
 export default async function Home() {
   return (
     <section className="h-screen flex items-center justify-center">
       <ClientComponentAuth />
+      <ServerComponentAuth />
     </section>
   );
 }

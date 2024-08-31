a8d47d51f6199e6eb5d6ea19aacc2552b1ec994c
flcampanelli <flcampanelli@gmail.com>
2024-08-31 14:59:53 -0300

    feat(navigation): add user avatar and dropdown menu to navigation bar

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -1,6 +1,20 @@
 import { getCurrentUser } from "@/lib/session";
+import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export async function NavigationBar() {
   const user = await getCurrentUser();
@@ -19,36 +33,76 @@ export async function NavigationBar() {
           Next Auth
         </Link>
         <div className="flex items-center gap-4">
-          <div className="flex items-center gap-x-1">
+          <div className="flex items-center gap-x-2">
             {!user ? (
               <>
                 <Link
                   href="/login"
-                  className="px-4 py-2 font-bold text-xs text-center text-gray-900 uppercase rounded-lg select-none 
-                  transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none
-                  disabled:opacity-50 disabled:shadow-none"
+                  className={cn(
+                    buttonVariants({ variant: "ghost", size: "sm" }),
+                    "hover:bg-gray-900/10",
+                    "active:bg-gray-900/20"
+                  )}
                 >
-                  <span>Entrar</span>
+                  Entrar
                 </Link>
                 <Link
                   href="/register"
-                  className="px-4 py-2 font-bold text-xs text-center text-white uppercase rounded-lg select-none 
-                  transition-all bg-gradient-to-tr from-gray-900 to-gray-800 shadow-md shadow-gray-900/10 
-                  hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none 
-                  disabled:opacity-50 disabled:shadow-none"
+                  className={cn(
+                    buttonVariants({ size: "sm" }),
+                    "hover:shadow-lg",
+                    "hover:shadow-gray-900/20",
+                    "active:opacity-[0.85]"
+                  )}
                 >
-                  <span>Registrar</span>
+                  Registrar
                 </Link>
               </>
             ) : (
-              <Link
-                href="/api/auth/signout"
-                className="px-4 py-2 font-bold text-xs text-center text-gray-900 uppercase rounded-lg select-none 
-                transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none
-                disabled:opacity-50 disabled:shadow-none"
-              >
-                <span>Sair</span>
-              </Link>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="relative h-8 w-8 rounded-full"
+                  >
+                    <Avatar className="h-8 w-8">
+                      <AvatarImage
+                        src={user.image ? user.image : ""}
+                        alt="user avatar"
+                      />
+                      <AvatarFallback>{user.name?.charAt(0)}</AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="end" forceMount>
+                  <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        {user.name}
+                      </p>
+                      <p className="text-xs leading-none text-muted-foreground">
+                        {user.email}
+                      </p>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                      <span>Perfil</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <span>Configurações</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <Link href="/api/auth/signout">
+                      <LogOut className="mr-2 h-4 w-4" />
+                      Sair
+                    </Link>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             )}
           </div>
         </div>

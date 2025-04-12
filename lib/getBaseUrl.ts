fc7aaf9b6920e170a85163988b9aa6f384ec402b
flcampanelli <flcampanelli@gmail.com>
2025-04-12 15:37:30 -0300

    refactor(lib): simplify base URL logic in getBaseUrl helper

--- lib/getBaseUrl.ts
+++ lib/getBaseUrl.ts
@@ -1,10 +1,7 @@
-import { headers } from "next/headers";
-
 export function getBaseUrl(): string {
-  const host = headers().get("host");
-  const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
-  if (!host) throw new Error("Host header is missing");
+  if (!baseUrl) throw new Error("Host header is missing");
 
-  return `${protocol}://${host}`;
+  return baseUrl;
 }

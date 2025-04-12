fffc90dc9affb554c120f2f6bebb7360ee43af8c
flcampanelli <flcampanelli@gmail.com>
2025-04-12 15:39:47 -0300

    feat(home): display message when no events are found

--- app/page.tsx
+++ app/page.tsx
@@ -29,16 +29,27 @@ export default async function Home() {
       className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 m-10 mx-auto px-4 sm:max-w-[40rem] md:max-w-[48rem]
         lg:max-w-[64rem] xl:max-w-[80rem]"
     >
-      {events.map(({ id, banner, title, date, placeName }) => (
-        <EventCard
-          key={id}
-          id={id}
-          banner={banner}
-          title={title}
-          date={formatDate(date)}
-          place={placeName}
-        />
-      ))}
+      {events.length ? (
+        events.map(({ id, banner, title, date, placeName }) => (
+          <EventCard
+            key={id}
+            id={id}
+            banner={banner}
+            title={title}
+            date={formatDate(date)}
+            place={placeName}
+          />
+        ))
+      ) : (
+        <div className="col-span-full text-center py-10">
+          <h2 className="text-2xl font-semibold mb-2">
+            Nenhum evento encontrado
+          </h2>
+          <p className="text-gray-500">
+            Não há eventos registrados no momento.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
